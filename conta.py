67531952ad17e10a91ff2aaec8731aa820035a89
lopesma11 <ml722090@gmail.com>
2023-12-26 15:55:39 -0300

    add getters and setters

--- conta.py
+++ conta.py
@@ -21,3 +21,18 @@ class Conta:
         self.saque(valor)
         conta_destino.deposito(valor)
 
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
